01873a3fec430354be5c95c3d760740b0ffc0ba2
Aryanmallik <aryanmallik2004@gmail.com>
2024-03-18 08:29:40 +0530

    draw

--- a.js
+++ a.js
@@ -3,24 +3,25 @@ const slider = document.querySelector(".slider");
 const reset = document.querySelector(".reset");
 const color = document.querySelector(".color");
 let isdraw = false;
+parent.ondragstart = function () {
+  return false;
+};
 function start() {
-  gridmake(600 / 16, slider.value);
+  gridmake(slider.value);
   slider.addEventListener("mouseup", () => {
     const slider = document.querySelector(".slider");
     let size = slider.value;
     console.log(slider.value);
-    var density = 600 / size;
-    gridmake(density, size);
+    gridmake(size);
   });
 
-  function gridmake(density, size) {
+  function gridmake(size) {
     parent.innerHTML = "";
     for (let i = 0; i < size; i++) {
       let row = document.createElement("div");
       for (let j = 0; j < size; j++) {
         let col = document.createElement("div");
         col.setAttribute("class", "col");
-        col.setAttribute("style", `width:${density}px;height:${density}px`);
         row.appendChild(col);
       }
       row.setAttribute("class", "row");
@@ -28,39 +29,45 @@ function start() {
     }
     const col = document.querySelectorAll(".col");
     col.forEach((pixel) => {
-      pixel.addEventListener("mousedown", function (event) {
-        if (event.buttons === 1) {
-          isdraw = true;
+      pixel.addEventListener("mousedown", () => {
+        draw(pixel);
+      });
+      pixel.addEventListener("mouseover", (e) => {
+        if (e.buttons === 1) {
           draw(pixel);
         }
-      });
-      pixel.addEventListener("mouseenter", function (event) {
-        if (isdraw && event.buttons === 1) {
-          draw(pixel);
-        }
-      });
-      pixel.addEventListener("mouseup", () => {
-        isdraw = false;
-      });
-      parent.addEventListener("mouseleave", () => {
-        isdraw = false;
       });
     });
 
+    // col.forEach((pixel) => {
+    //   pixel.addEventListener("mousedown", function (event) {
+    //     if (event.buttons === 1) {
+    //       isdraw = true;
+    //       draw(pixel);
+    //     }
+    //   });
+    //   pixel.addEventListener("mouseenter", function (event) {
+    //     if (isdraw && event.buttons === 1) {
+    //       draw(pixel);
+    //     }
+    //   });
+    //   pixel.addEventListener("mouseup", () => {
+    //     isdraw = false;
+    //   });
+    //   parent.addEventListener("mouseleave", () => {
+    //     isdraw = false;
+    //   });
+    // });
+
     draw = (pixel) => {
       colorPick = color.value;
-      pixel.setAttribute(
-        "style",
-        `background-color:${colorPick};width:${density}px;height:${density}px`
-      );
+      pixel.setAttribute("style", `background-color:${colorPick}`);
     };
   }
   reset.addEventListener("mousedown", () => {
     const slider = document.querySelector(".slider");
     let size = slider.value;
-    console.log(slider.value);
-    var density = 600 / size;
-    gridmake(density, size);
+    gridmake(size);
   });
 }
 start();
